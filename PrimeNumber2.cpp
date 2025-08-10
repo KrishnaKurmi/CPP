e75973df2cae0eb166a55398daf8ac850472cd54
unknown <hollybuddy.ts2206@gmail.com>
2025-08-10 13:10:06 +0530

    Create PrimeNumber2.cpp

---
+++ PrimeNumber2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num <= 1) {
+        cout << num << " is NOT a prime number." << endl;
+    } else {
+        bool isPrime = true;
+
+        for (int i = 2; i <= sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            cout << num << " is a Prime Number." << endl;
+        else
+            cout << num << " is NOT a Prime Number." << endl;
+    }
+
+    return 0;
+}
